879f44d51683a496fe8396517f6c8664221b622b
ikenley <ikenley6@gmail.com>
2021-07-16 08:39:48 -0400

    log to stdout

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -1,7 +1,5 @@
-import logging
-from logging.handlers import RotatingFileHandler
-import os
 from flask import Flask, jsonify
+from logging.config import dictConfig
 from config import Config
 
 
@@ -18,18 +16,21 @@ def create_app(config_class=Config):
     from api.api import bp as api_bp
     app.register_blueprint(api_bp)
 
-    # if not app.debug and not app.testing:
-    #     if not os.path.exists('logs'):
-    #         os.mkdir('logs')
-    #     file_handler = RotatingFileHandler('logs/microblog.log',
-    #                                        maxBytes=10240, backupCount=10)
-    #     file_handler.setFormatter(logging.Formatter(
-    #         '%(asctime)s %(levelname)s: %(message)s '
-    #         '[in %(pathname)s:%(lineno)d]'))
-    #     file_handler.setLevel(logging.INFO)
-    #     app.logger.addHandler(file_handler)
-
-    #     app.logger.setLevel(logging.INFO)
-    #     app.logger.info('Microblog startup')
+    # Log to stdout
+    dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }},
+        'handlers': {'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+        }
+    })
 
     return app
